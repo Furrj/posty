3eda4c1591f490c615141fd3c4cfe312ebdf15b2
Jackson Furr <jackson.a.furr@gmail.com>
2024-01-11 18:59:18 -0500

    need to fix user_name out of doc flow

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const classes_cont = document.getElementById("classes_cont");
 
 async function main() {
     const allInfo: T_AllInfo = await get_storage_data_on_init();
-    user_name.innerHTML = allInfo.user_name;
+    user_name.innerText = allInfo.user_name;
     for (const cl of allInfo.classes) {
         const class_box = document.createElement("div");
         class_box.classList.add("class_box");
