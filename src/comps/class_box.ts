dfd926aaa061501522aea580e8ca183808b3031f
Jackson Furr <jackson.a.furr@gmail.com>
2024-01-12 00:23:57 -0500

    added checkboxes

--- src/comps/class_box.ts
+++ src/comps/class_box.ts
@@ -6,16 +6,18 @@ const class_box = (cl: T_ClassInfo, id: number): HTMLDivElement => {
     class_box.innerHTML = cl.class_name;
     class_box.id = `class_box_${id}`;
 
+    const progress_cont = document.createElement("div");
+    progress_cont.id = "progress_cont";
+    class_box.appendChild(progress_cont);
+
     const progress_box = document.createElement("div");
     progress_box.classList.add("progress_box");
-    class_box.appendChild(progress_box);
+    progress_cont.appendChild(progress_box);
 
     const post_check = document.createElement("input");
     post_check.type = "checkbox";
-    post_check.innerHTML = "Post";
     post_check.classList.add("check");
     post_check.id = `post_check_${id}`;
-    post_check.checked = true;
     progress_box.appendChild(post_check);
 
     const label = document.createElement("label");
@@ -23,7 +25,39 @@ const class_box = (cl: T_ClassInfo, id: number): HTMLDivElement => {
     label.classList.add("label");
     label.innerText = "Post";
     progress_box.appendChild(label);
-    class_box.appendChild(progress_box);
+
+    const progress_box_2 = document.createElement("div");
+    progress_box_2.classList.add("progress_box");
+    progress_cont.appendChild(progress_box_2);
+
+    const response_1_check = document.createElement("input");
+    response_1_check.type = "checkbox";
+    response_1_check.classList.add("check");
+    response_1_check.id = `response_1_check_${id}`;
+    progress_box_2.appendChild(response_1_check);
+
+    const label_2 = document.createElement("label");
+    label_2.htmlFor = `response_1_check_${id}`;
+    label_2.classList.add("label");
+    label_2.innerText = "Response 1";
+    progress_box_2.appendChild(label_2);
+
+    //3
+    const progress_box_3 = document.createElement("div");
+    progress_box_3.classList.add("progress_box");
+    progress_cont.appendChild(progress_box_3);
+
+    const response_2_check = document.createElement("input");
+    response_2_check.type = "checkbox";
+    response_2_check.classList.add("check");
+    response_2_check.id = `response_2_check_${id}`;
+    progress_box_3.appendChild(response_2_check);
+
+    const label_3 = document.createElement("label");
+    label_3.htmlFor = `response_2_check_${id}`;
+    label_3.classList.add("label");
+    label_3.innerText = "Response 2";
+    progress_box_3.appendChild(label_3);
 
     return class_box;
 };
